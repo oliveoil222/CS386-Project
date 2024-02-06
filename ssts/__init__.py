2150a1df1a0eff1f736e9bc415206c61a1487cd4
Charles Descamps <128670210+cmd644@users.noreply.github.com>
2024-02-05 19:34:28 -0700

    Commented out and fixed file __init__.py

--- ssts/__init__.py
+++ ssts/__init__.py
@@ -38,6 +38,8 @@ def create_app(test_config=None):
 
     @app.route('/login')
     def agent_login_page(page_name="Agent Login"):
+        # low priority TODO, will use Wertzeug's libraries to
+        # implement a login.
         return render_template("auth/login.html", page_name=page_name)
 
     @app.route('/sp')
@@ -46,14 +48,25 @@ def create_app(test_config=None):
 
     @app.route('/sp/schedule')
     def service_portal_schedule(page_name="Service Portal Scheduler"):
+        # medium priority TODO, will have a scheduler on this page.
         return render_template("service_portal/schedule.html", page_name=page_name)
 
     @app.route('/new/ticket')
     def new_ticket(page_name="New Ticket"):
+        # will need to save the DB information from the webpage
+        # probably using an API request template once the submit button was hit.
+        # will need to get the information from front-end about what information
+        # was on the page.
+        # this function may also need to generate the ID Number of the ticket
+        # and render it with the page as immutable.
         return render_template("new/ticket.html", page_name=page_name)
 
-    @app.route('/view/ticket/[ID_NUMBER]')
-    def new_ticket(page_name="View Ticket"):
-        return render_template("view/ticket/[ID_NUMBER].html", page_name=page_name)
+    @app.route('/view/ticket/<int:id_number>')
+    def view_ticket(page_name="View Ticket {id_number}", ticket_id=id_number):
+        # may need to be modifed, flask might do something with this
+        # that may be unforeseeable without a bit of testing...
+        # will need a template base for ticket displaying from the frontend.
+        # the rest of the function will take DB functions to get the information.
+        return render_template("view/ticket/{ticket_id}.html", page_name=page_name)
 
     return app
