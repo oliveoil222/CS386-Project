fe47850cdbe8efa6ba64145823ffeb6c4d8ec42c
Sam Cain <src556@nau.edu>
2024-02-23 16:01:23 -0700

    finished counting for website

--- ssts/__init__.py
+++ ssts/__init__.py
@@ -7,16 +7,27 @@ from flask import request
 from flask import render_template
 from markupsafe import escape
 
+id_number = "000"
 
 def create_app(test_config=None):
+    global id_number
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'ssts.mongodb'),
     )
-
-
+    # pull out the ID number of ticket
+    # check database for last number used, if any
+    try:
+        # do database stuff we expect to work here
+        # like pulling last ID number
+        # if this errors, then move to except
+        print("working database stuff here")
+    except:
+        id_number = "001"
+        # then this is where we set the first ID number for initialization
+        # note to programmer: use str.zfill(3) to get three digit integer numbers
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
@@ -31,6 +42,7 @@ def create_app(test_config=None):
     except OSError:
         pass
 
+
     # all application routes
     @app.route('/')
     def homepage(page_name="Homepage"):
@@ -40,7 +52,7 @@ def create_app(test_config=None):
     def agent_login_page(page_name="Agent Login"):
         return render_template("auth/login.html", page_name=page_name)
 
-    @app.route('/sp')
+    @app.route('/service')
     def service_portal(page_name="Service Portal"):
         return render_template("service_portal/portal.html", page_name=page_name)
 
@@ -48,12 +60,48 @@ def create_app(test_config=None):
     def service_portal_schedule(page_name="Service Portal Scheduler"):
         return render_template("service_portal/schedule.html", page_name=page_name)
 
-    @app.route('/new/ticket')
-    def new_ticket(page_name="New Ticket"):
-        return render_template("new/ticket.html", page_name=page_name)
+    @app.route('/new')
+    def new_ticket_home(page_name="New Ticket Home"):
+        # will essentially redirect to the ticketing page
 
-    @app.route('/view/ticket/[ID_NUMBER]')
-    def new_ticket(page_name="View Ticket"):
-        return render_template("view/ticket/[ID_NUMBER].html", page_name=page_name)
+        # render template will need to be changed in a later revision.
+
+        return render_template("new/ticket.html", page_name=page_name, id_number=id_number)
+
+    @app.route('/new/ticket', methods=['GET','POST'])
+    def new_ticket(page_name="New Ticket {id_number}"):
+        global id_number
+
+        if request.method == 'POST':
+        # Process form data and save the ticket to the database
+        # Increment the current page number by one
+            id_number = str(int(id_number) + 1)
+        return render_template("new/ticket.html", page_name=page_name, id_number=id_number)
+
+    @app.route('/view')
+    def view_ticket_home(page_name="Ticket Home"):
+        # this will render a page for the user to click between
+        # either using the list view or the ID number directly.
+
+        return render_template("view/ticket", page_name=page_name)
+
+    @app.route('/view/ticket')
+    def view_ticket_list(page_name="View Tickets"):
+        # this will return a list of the past X tickets and render to user
+
+        return render_template("view/ticket", page_name=page_name)
+
+    @app.route('/view/ticket/<string:id_number>')
+    def view_ticket(page_name="View Ticket {id_number}"):
+        # may need to be modifed, flask might do something with this
+        # that may be unforeseeable without a bit of testing...
+        # will need a template base for ticket displaying from the frontend.
+        # the rest of the function will take DB functions to get the information.
+
+        # get relevant information on the ticket
+            # includes id number, client email(s), worker email(s)
+            # work notes in reverse order timestamped (last comment first)
+
+        return render_template("view/ticket/{id_number}.html", page_name=page_name)
 
     return app
