88245ef33872bd93a14e972de4a8356531e20a60
Hannah Penado <hannahpenado@gmail.com>
2024-02-24 20:28:32 -0700

    updating main forked branch

---
+++ ssts/backendDB/dbClasses.py
@@ -0,0 +1,28 @@
+import sys
+sys.path.insert(0, './')
+import addInfo as db
+
+# class to initialize connections to mongoDB
+    # method to connect to mongo
+    # method to connect to ticket collection
+    # method to conenct to worker collection
+    # method to connect to device collection
+    # method to connect to team collection
+    # method to connect to clients collection
+    # method to connect to solutions collection
+
+# class to keep track of document id count in each collection
+    # method to inc ticket id
+    # method to inc worker id
+    # method to inc device id
+    # method to inc team id
+    # method to inc client id
+    # method to inc solution id
+
+    # method to grab last ticket doc
+    # method to grab last worker doc
+    # method to grab last device doc
+    # method to grab last team doc
+    # method to grab last client doc
+    # method to grab last solution doc
+
