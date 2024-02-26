73dc5529c8d6ddebf4b90f151abd234b5cf96c27
Dallon Jarman <116462823+dallonjarman@users.noreply.github.com>
2024-02-26 14:43:06 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ from ssts import create_app
 
 # Call the application factory function to construct a Flask application
 # instance using the development configuration
-app = create_app('flask.cfg')
+app = create_app( )
